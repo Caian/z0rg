8f5d0067d62fdc142b4f726bbd94a7254a2e523a
Caian Benedicto <caianbene@gmail.com>
2020-03-07 10:22:45 -0300

    Fix license headers

--- test/test1.cpp
+++ test/test1.cpp
@@ -14,8 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GNU Make; see the file COPYING.  If not, write to
- * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ * along with z0rg.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "z0rg/zero_copy.hpp"
